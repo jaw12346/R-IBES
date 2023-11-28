aa314ad6435aae3a56009680e3db7794e027225f
Jacob <jaw12346@gmail.com>
2023-11-27 20:18:32 -0500

    Updated benchmarking script and requirements file

--- src/benchmark-local_facial_recognition.py
+++ src/benchmark-local_facial_recognition.py
@@ -4,10 +4,12 @@ R-IBES facial recognition benchmarking script
 
 import sqlite3
 import time
+import statistics
 from collections import namedtuple
+from alive_progress import alive_bar
 
-from src import local_facial_recognition as lfr
-from src import conversions
+import local_facial_recognition as lfr
+import conversions
 
 BenchmarkResult = namedtuple('BenchmarkResult',
                              ['match_count', 'fail_count', 'success_rate', 'compare_time'])
@@ -42,6 +44,32 @@ def get_encodings():
     return None
 
 
+def stats(success_rates, avg_times, num_zero_success_rates, number_of_people, avg_non_zero_success_rates):
+    """
+    Calculate the statistics for the benchmark.
+
+    :param success_rates: List of success rates for each person
+    :type success_rates: list(float)
+    :param avg_times: List of average times to compare all encodings for each person
+    :type avg_times: list(float)
+    :param num_zero_success_rates: Number of people with 0% success rate
+    :type num_zero_success_rates: int
+    :param number_of_people: Total number of people in the database
+    :type number_of_people: int
+    :param avg_non_zero_success_rates: List of success rates for each person excluding those with 0% success rates
+    :type avg_non_zero_success_rates: list(float)
+    :return: Average success rate, average encoding comparison time, percent of people with 0% success rate,
+             and average non-zero success rate
+    :rtype: float, float, float, float
+    """
+    average_success_rate = sum(success_rates) / len(success_rates)
+    average_time = sum(avg_times) / len(avg_times)
+    percent_zero_success_rates = num_zero_success_rates / number_of_people
+    average_non_zero_success_rate = sum(avg_non_zero_success_rates) / len(avg_non_zero_success_rates)
+
+    return average_success_rate, average_time, percent_zero_success_rates, average_non_zero_success_rate
+
+
 def benchmark():
     """
     Benchmark the local facial recognition system.
@@ -51,53 +79,83 @@ def benchmark():
     encodings = get_encodings()
     all_results = {}  # Name : CompareResult
 
-    lowest_success_rate = 1
-    lowest_success_person = ''
-    highest_success_rate = 0
-    highest_success_person = ''
+    avg_times = []
+    success_rates = []
+    num_zero_success_rates = 0
+    number_of_people = len(encodings)
+    avg_non_zero_success_rates = []
 
     all_start = time.time()
     current_num = 1
     # total number of elements from all encodings
-    for name in encodings:
-        match_count = 0
-        fail_count = 0
-        person_encodings = encodings[name]
-        times = []
-        for encoding in person_encodings:
-            # For each encoding, compare it to every other encoding in the database
-            start_time = time.time()
-            determined_person = lfr.identify_person_from_encoding(encoding)
-            if determined_person == name:
-                match_count += 1
+    with alive_bar(len(encodings), force_tty=True) as bar:  # Progress bar, force_tty=True for PyCharm
+        for name in encodings:
+            match_count = 0
+            fail_count = 0
+            person_encodings = encodings[name]
+            times = []
+            for encoding in person_encodings:
+                # For each encoding, compare it to every other encoding in the database
+                start_time = time.time()
+                determined_person = lfr.identify_person_from_encoding(encoding)
+                if determined_person == name:
+                    match_count += 1
+                else:
+                    fail_count += 1
+                comparison_time = time.time() - start_time
+                times.append(comparison_time)
+            avg_times.append(sum(times) / len(times))  # Average time to compare all encodings for this person
+            success_rate = match_count / (match_count + fail_count)
+            success_rates.append(success_rate)
+            if success_rate == 0:
+                num_zero_success_rates += 1
             else:
-                fail_count += 1
-            comparison_time = time.time() - start_time
-            times.append(comparison_time)
-        success_rate = match_count / (match_count + fail_count)
-        if success_rate < lowest_success_rate:
-            lowest_success_rate = success_rate
-            lowest_success_person = name
-        if success_rate > highest_success_rate:
-            highest_success_rate = success_rate
-            highest_success_person = name
+                avg_non_zero_success_rates.append(success_rate)
 
-        all_results[name] = BenchmarkResult(match_count, fail_count, success_rate, times)
-        print(f'{current_num} / {len(encodings)} -- {(time.time() - all_start):.0f} seconds total')
-        current_num += 1
+
+            all_results[name] = BenchmarkResult(match_count, fail_count, success_rate, times)
+            current_num += 1
+            bar()  # Update progress bar
     total_time = time.time() - all_start
 
-    with open('./benchmarks/local_facial_recognition_benchmark_results.txt', 'w') as outfile:
+    average_success_rate, average_time, percent_zero_success_rates, average_non_zero_success_rate = (
+        stats(success_rates, avg_times, num_zero_success_rates,
+              number_of_people, avg_non_zero_success_rates))
+
+
+    with open('./benchmarks/local_facial_recognition_benchmark_results.tsv', 'w') as outfile:
+        outfile.write('Name\tMatch_Count\tFail_Count\tSuccess_Rate\tCompare_Times\n')  # Header
         for name, result in all_results.items():
             outfile.write(f'{name}\t{result.match_count}\t{result.fail_count}\t{result.success_rate}\t'
-                          f'{result.compare_time}\n')
+                          f'{result.compare_time}\n')  # Data / rows
         outfile.write('-'*50 + '\n')
-        outfile.write(f'Lowest success rate: {lowest_success_rate:.5f}% ({lowest_success_person})\n')
-        outfile.write(f'Highest success rate: {highest_success_rate:.5f}% ({highest_success_person})\n')
-        outfile.write(f'Total time to compare all encodings: {total_time:.0f} seconds\n')
 
-    print(f'Lowest success rate: {lowest_success_rate:.5f}% ({lowest_success_person})')
-    print(f'Highest success rate: {highest_success_rate:.5f}% ({highest_success_person})')
+        # General statistics
+        outfile.write(f'Average image search success rate (using local DB): {average_success_rate*100:.3f}%')
+        outfile.write(f'\tStandard deviation: '
+                      f'{statistics.stdev(success_rates, xbar=average_success_rate)*100:.3f}%')
+
+        outfile.write(f'Percent of people with 0% success rate: {percent_zero_success_rates*100:.3f}%')
+        outfile.write(f'Average image search success rate (using local DB) excluding 0% success rates: '
+              f'{average_non_zero_success_rate*100:.3f}%')
+        outfile.write(f'\tStandard deviation: '
+                      f'{statistics.stdev(avg_non_zero_success_rates, xbar=percent_zero_success_rates)*100:.3f}%')
+
+        outfile.write(f'Average time per image search: {average_time:.3f} seconds')
+        outfile.write(f'\tStandard deviation: {statistics.stdev(avg_times, xbar=average_time):.3f}')
+        outfile.write(f'Total time to compare all encodings: {total_time:.0f} seconds')
+
+    print(f'Average image search success rate (using local DB): {average_success_rate*100:.3f}%')
+    print(f'\tStandard deviation: {statistics.stdev(success_rates, xbar=average_success_rate) * 100:.3f}%')
+
+    print(f'Percent of people with 0% success rate: {percent_zero_success_rates * 100:.3f}%')
+    print(f'Average image search success rate (using local DB) excluding 0% success rates: '
+          f'{average_non_zero_success_rate*100:.3f}%')
+    print(f'\tStandard deviation: '
+          f'{statistics.stdev(avg_non_zero_success_rates, xbar=percent_zero_success_rates)*100:.3f}%')
+
+    print(f'Average time per image search: {average_time:.3f} seconds')
+    print(f'\tStandard deviation: {statistics.stdev(avg_times, xbar=average_time):.3f}')
     print(f'Total time to compare all encodings: {total_time:.0f} seconds')
 
 
