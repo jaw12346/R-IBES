d3cfd9eb07deea2dd698ad44dbfbccba02193911
Jacob <jaw12346@gmail.com>
2024-02-25 17:53:08 -0500

    Wrote a getter function get_ontologies_from_db() which returns a list of all ontologies. Wrote get_word_variations() to get variations of a word within a query. For example, with a query of 'birth place location' and index 0, it will return ['birth', 'birthplace', 'birth place']. This is helpful for word similarity checks where an ontology may be a compound word or a short phrase. The file query_conversion.py will act as a staging ground for query NLP work.

---
+++ src/query_conversion.py
@@ -0,0 +1,49 @@
+"""
+The purpose of this file is to provide a space for drafting methods necessary for
+converting user queries into exclusively ontologies.
+"""
+import sqlite3
+
+
+def get_ontologies_from_db():
+    """
+    Get the ontologies from the database.
+
+    :return: All ontologies from the ONTOLOGIES table in the database (sorted alphabetically).
+    :rtype: list(str)
+    """
+    conn = sqlite3.connect('./r-ibes.db')
+    print('Opened database successfully')
+    cursor = conn.execute('SELECT ONTOLOGY FROM ONTOLOGIES ORDER BY ONTOLOGY ASC')
+    ontologies = cursor.fetchall()
+    ontologies = [value[0] for value in ontologies]
+    conn.close()
+    return ontologies
+
+
+def get_word_variations(index, query):
+    """
+    Get the variations of a word in a query.
+    Example: index=0, query='birth place location' -> ['birth', 'birthplace']
+    Example: index=1, query='birth place location' -> ['place', 'placelocation']
+    Example: index=2, query='birth place location' -> ['location']
+
+    :param index: Index of the word in the query.
+    :type index: int
+    :param query: Complete query.
+    :type query: str
+    :return: Variations of the word in the query.
+    :rtype: list(str)
+    """
+    curr_word = query[index]
+    results = [curr_word]
+    if index+1 >= len(query):
+        return results
+    results.append(curr_word + query[index + 1])
+    results.append(curr_word + ' ' + query[index + 1])
+    return results
+
+
+if __name__ == '__main__':
+    query = 'areacode of birth place'
+    # queries_to_ontologies(query)
